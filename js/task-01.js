c8d448b98c79b08ca702667ec6d8fe57b45c17b9
Devysna <yashturman@gmail.com>
2023-02-18 00:10:38 +0200

    task-01 рішення

--- js/task-01.js
+++ js/task-01.js
@@ -10,12 +10,7 @@
 const categoriesItemRef = categories.querySelectorAll('.item');
 console.log('Number of categories:',categoriesItemRef.length);
 
-
-
-const titleItemRefs = categories.querySelectorAll('h2');
-console.log(titleItemRefs);
-console.log('Category:',titleItemRefs.textContent);
-
-
-const listItemRef = categories.querySelectorAll('li.item.ul.li');
-console.log('Elements:',listItemRef);
+categoriesItemRef.forEach(item => {
+       console.log('Category:',item.firstElementChild.textContent);
+       console.log('Elements:',item.lastElementChild.children.length)
+    });
